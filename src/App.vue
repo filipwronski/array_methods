2e0cf4a430d4724effcdd037996804c80c5e1f32
filip <filip0822@gmail.com>
2020-01-09 22:43:55 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,82 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+  },
+  mounted: function() {
+    const testArray = ['test', 'test1', 'test3']
+    console.log(testArray.__proto__)
+    
+    this.testConcat()
+    this.testCopyWithin()
+    this.testEnteries()
+    this.testEvery()
+    this.testFill()
+    this.testFilter()
+    this.testFind()
+    this.testFindIndex()
+  },
+  methods: {
+    testConcat() {
+      // arr.concat - połącz nie nadpisując
+      const testArray = ['test', 'test1', 'test3']
+      console.log(testArray.concat(['test4']))
+    },
+    testCopyWithin() {
+      // arr.copyWithin - kopiuj wewnątrz i zamień nadpisując
+      const testArray = ['test', 'test1', 'test3']
+      console.log(testArray.copyWithin(0, 2, 3))
+    },
+    testEnteries() {
+      // arr.entries - zwraca iterator zawierający [index, wartosc]
+      const testArray = ['test', 'test1', 'test3']
+      const iterator = testArray.entries()
+      console.log(iterator.next().value)
+    },
+    testEvery() {
+      // arr.entries - przyjmuje metodę która sprawdza czy każdy element przechodzi test
+      const testArray = ['test', 'test1', 'test3']
+      const isTest = (element, index, array) => {
+          return element === 'test'
+      }
+      console.log(testArray.every(isTest))
+    },
+    testFill() {
+      // arr.fill(val, indexS, indexE) - wypełnia tablice podaną wartością od indexS do indexE
+      const testArray = ['test', 'test1', 'test3']
+      console.log(testArray.fill(0, 1, 3))
+    },
+    testFilter() {
+      const testArray = ['test', 'test', 'test3']
+      const isTest = (element, index, array) => {
+        return element === 'test'
+      } 
+      // arr.filter()
+      console.log(testArray.filter(isTest))
+
+    },
+    testFind() {
+      const testArray = ['test', 'test', 'test3']
+      const isTest = (element, index, array) => {
+        return element === 'test'
+      } 
+      // arr.filter()
+      console.log(testArray.find(isTest))
+    },
+    testFindIndex() {
+      const testArray = ['test', 'test', 'test3']
+      const isTest = (element, index, array) => {
+        return element === 'test3'
+      } 
+      // arr.filter()
+      console.log(testArray.findIndex(isTest))
+    }
   }
 }
 </script>
